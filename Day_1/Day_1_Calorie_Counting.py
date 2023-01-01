233af7ec78973ee9feab9891670b64d7663e2fe0
gabisala <sala.gab@gmail.com>
2023-01-01 18:49:24 +0200

    solutions for day 1

--- Day_1/Day_1_Calorie_Counting.py
+++ Day_1/Day_1_Calorie_Counting.py
@@ -37,7 +37,49 @@ The second Elf is carrying one food item with 4000 Calories.
 The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
 The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
 The fifth Elf is carrying one food item with 10000 Calories.
-In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like 
+to know how many Calories are being carried by the Elf carrying the most Calories. 
+In the example above, this is 24000 (carried by the fourth Elf).
 
 Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+--- Part Two ---
+By the time you calculate the answer to the Elves' question, they've already realized that the Elf 
+carrying the most Calories of food might eventually run out of snacks.
+To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried 
+by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, 
+they still have two backups.
+In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), 
+then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 """
+
+# Process input data
+with open('Day_1/input.txt', 'r') as file:
+    lines = file.readlines()
+
+elfs_food = []
+food = []
+for line in lines:
+    if line != '\n':
+        food.append(int(line.rstrip('\n')))
+    else:
+        elfs_food.append(food)
+        food = []
+elfs_food.append(food)
+
+# First puzzle
+def carrying_most_calories(elfs_food):
+    return max([sum(food_calories) for food_calories in elfs_food])
+
+most_calories = carrying_most_calories(elfs_food)
+print(most_calories)
+
+# Second puzzle
+def sum_top_3_most_calories(elfs_food):
+    all_backpacks = [sum(food_calories) for food_calories in elfs_food]
+    sorted_backpacks = sorted(all_backpacks, reverse=True)
+    return sum(sorted_backpacks[:3])
+
+sum_of_top_3_calories = sum_top_3_most_calories(elfs_food)
+print(sum_of_top_3_calories)
